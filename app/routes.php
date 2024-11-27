a7d2f1cf9cd47c0c8d41827dbf766259efde501f
Jhonatan <jhonatan.moraes_iebt@123milhas.com.br>
2024-11-27 00:04:16 -0300

    first commit

---
+++ app/routes.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
+    $app->get('/numero_extenso/{numero}', function (Request $request, Response $response, $args) {
+        $formatter  = new \NumberFormatter('PT_BR', NumberFormatter::SPELLOUT);
+        $numero_extenso = $formatter->format((int) $args['numero']);
+        $response->getBody()->write(json_encode($numero_extenso));
+        return $response->withHeader('Content-Type', 'application/json');
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+};
